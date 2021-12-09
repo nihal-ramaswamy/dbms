fdc945ba265b26ec447b2e539426a214e9040dfc
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2021-12-09 21:53:08 +0530

    Added multiple fields

--- app.py
+++ app.py
@@ -31,7 +31,31 @@ def exec(role, query):
 def main_menu():
     roles = ["emp", "mover", "cust", "admin", "accountant", "hr"]
     role = st.sidebar.selectbox("Role",roles)
-    query=st.text_input("Enter query",key='query')
+    op=st.text_input("Enter operation",key='op')
+    tbname=st.text_input("Enter table name",key='tbname')
+    where_cond=st.text_input("Enter where condition",key='where_cond')
+    set_cond = st.text_input("Enter set condition",key='set_cond')
+    password = st.text_input("Enter password",key='password')
+
+    query = ""
+
+    if (op == "SELECT"):
+        query = f"SELECT * FROM {tbname}"
+        if (where_cond is not None):
+            query = f"{query} WHERE {where_cond}"
+    elif (op == "INSERT"):
+        query = f"INSERT INTO {tbname}"
+        if (where_cond is not None):
+            query = f"{query} WHERE {where_cond}"
+    elif (op == "DELETE"):
+        query = f"DELETE FROM {tbname}"
+        if (where_cond is not None):
+            query = f"{query} WHERE {where_cond}"
+    elif (op == "UPDATE"):
+        query = f"UPDATE {tbname} SET {set_cond}"
+        if (where_cond is not None):
+            query = f"{query} WHERE {where_cond}"
+
     st.markdown(full_message_temp.format(f"{query} {role}"), unsafe_allow_html=True)
     value = exec(role, query)
     st.markdown(full_message_temp.format(f"{value}"), unsafe_allow_html=True)
